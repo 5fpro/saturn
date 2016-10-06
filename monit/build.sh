91d8b022adcbef42bc38425e0a1511a8ff0518f2
marsz <marsz330@gmail.com>
2016-10-07 00:36:04 +0800

    complete

--- monit/build.sh
+++ monit/build.sh
@@ -2,8 +2,12 @@ echo "Your app name? (lowcase)"
 read APP_NAME
 
 unicorn_file="/etc/init.d/unicorn-${APP_NAME}"
-curl -o $unicorn_file -sSL https://gist.githubusercontent.com/marsz/32a8dead9373df326d7bf5fe27d92b89/raw/bbf749e6ec39a82fe9728624cb3dde76b7894266/unicorn.sh
+curl -o $unicorn_file -sSL https://saturn.5fpro.com/monit/unicorn_bin.sh
 chmod +x $unicorn_file
+
+unicorn_conf="/etc/monit/conf.d/unicorn-${APP_NAME}"
+curl -o $unicorn_conf -sSL https://saturn.5fpro.com/monit/unicorn_monitrc.conf
+sed -i "s@{{UNICRON_BIN_FILE}}@${unicorn_file}@" $unicorn_conf
 
 echo "Your app path?"
 read APP_ROOT
@@ -33,9 +37,16 @@ echo "unicorn pid file path? (${PID_PATH}/unicorn.pid)"
 read UNICORN_PID
 if [ "$UNICORN_PID" == "" ]; then UNICORN_PID="${PID_PATH}/unicorn.pid"; fi;
 sed -i "s@{{UNICORN_PID}}@${UNICORN_PID}@" $unicorn_file
+sed -i "s@{{UNICORN_PID}}@${UNICORN_PID}@" $unicorn_conf
 
 echo "unicorn config file path? (${APP_ROOT}/config/unicorn/${RAILS_ENV}.rb)"
 read UNICORN_CONFIG_FILE
 if [ "$UNICORN_CONFIG_FILE" == "" ]; then UNICORN_CONFIG_FILE="${APP_ROOT}/config/unicorn/${RAILS_ENV}.rb"; fi;
 sed -i "s@{{UNICORN_CONFIG_FILE}}@${UNICORN_CONFIG_FILE}@" $unicorn_file
 
+echo "generating bin file in ${unicorn_file}"
+echo "generating conf file to %{unicorn_conf}"
+echo "append '${unicorn_file} start' to /etc/rc.local"
+echo "'${unicorn_file} start" >> /etc/rc.local
+echo "restarting monit..."
+/etc/init.d/monit reload
