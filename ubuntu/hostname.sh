562686fc874f5a0341494df8fe6e7e37e10a0e46
marsz <marsz330@gmail.com>
2017-03-10 04:37:13 +0800

    fix

--- ubuntu/hostname.sh
+++ ubuntu/hostname.sh
@@ -3,10 +3,6 @@ read hname
 
 if [[ $hname != "" ]]; then
   line="127.0.0.1 $hname"
-  if grep -q "${line}" "/etc/hosts"; then
-    echo "exists"
-  else
-    echo "\n$line" >> "/etc/hosts"
-  fi;
+  if grep -q "${line}" "/etc/hosts"; then echo "exists"; else echo "\n$line" >> "/etc/hosts"; fi;
   hostname $hname
 fi;
