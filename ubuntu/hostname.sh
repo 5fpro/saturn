84a799dad0b0884742467eb05e45ebfd6fa7650c
marsz <marsz330@gmail.com>
2017-04-01 14:36:00 +0800

    color for login banner

--- ubuntu/hostname.sh
+++ ubuntu/hostname.sh
@@ -1,9 +1,12 @@
-echo "Enter your server identify(name)"
+echo "Enter your server identify(name), without space"
 read hname
+
+apt-get install -y figlet toilet
 
 if [[ $hname != "" ]]; then
   line="127.0.0.1 $hname"
   if grep -q "${line}" "/etc/hosts"; then echo "exists"; else echo "$line" >> "/etc/hosts"; fi;
   hostname $hname
+  toilet --gay -f standard $hname > /etc/motd
   echo 'done!'
 fi;
