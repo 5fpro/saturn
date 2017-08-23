d1781e961eacbe59b03b6e93b339ffd6859c37ba
marsz <marsz330@gmail.com>
2017-08-24 02:37:07 +0800

    ssl renew

--- ssl/renew.sh
+++ ssl/renew.sh
@@ -2,7 +2,8 @@ echo "Domain name?"
 read DOMAIN_NAME
 filename=(${DOMAIN_NAME// \-d/ })
 cron_file="/etc/cron.weekly/ssl-renewal-${filename}"
-renew_cmd="(/etc/dehydrated/dehydrated -c -d ${DOMAIN_NAME}) && (/etc/init.d/nginx restart)"
+renew_cmd="/etc/dehydrated/dehydrated -c -d ${DOMAIN_NAME}\
+/etc/init.d/nginx restart"
 touch $cron_file
 echo "#!/bin/sh" >> $cron_file
 echo $renew_cmd >> $cron_file
