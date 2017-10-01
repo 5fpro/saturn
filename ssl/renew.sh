c35203ccac3e394c66e5e81329db0992ba1092bc
marsz <marsz330@gmail.com>
2017-10-02 00:07:13 +0800

    dont use dot as filename

--- ssl/renew.sh
+++ ssl/renew.sh
@@ -1,6 +1,7 @@
 echo "Domain name?"
 read DOMAIN_NAME
 filename=(${DOMAIN_NAME// \-d/ })
+filename=`echo $filename|sed 's/\./\-/g'`
 cron_file="/etc/cron.weekly/ssl-renewal-${filename}"
 renew_cmd="/etc/dehydrated/dehydrated -c -d ${DOMAIN_NAME}\
 /etc/init.d/nginx restart"
